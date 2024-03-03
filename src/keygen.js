ec262c801dadf79ab36e58019a16d268a06f3652
The Maximalist <hello@themaximalist.com>
2024-03-02 18:06:05 -0800

    base url

--- src/keygen.js
+++ src/keygen.js
@@ -2,7 +2,7 @@ import Keygen from "@themaximalist/keygen.js"
 
 const keygen = new Keygen({
     account_id: process.env.KEYGEN_ACCOUNT_ID,
-    ignore_ssl: true,
+    base_url: process.env.KEYGEN_BASE_URL,
 });
 
 export default keygen;
