58ecaeaca80005aaa1f24edf0d831c52f0da92ed
The Maximalist <hello@themaximalist.com>
2024-03-02 18:01:32 -0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+APP_NAME="licenses.cac.app"
+
+# Check if the first argument is "--deploy"
+if [[ $1 == "--deploy" ]]; then
+  FORCE_DEPLOY=true
+else
+  FORCE_DEPLOY=false
+fi
+
+UPDATES=$(git pull)
+
+# Check if updates are found or if forced to deploy
+if [[ $UPDATES != *"Already up to date"* ]] || [[ $FORCE_DEPLOY == true ]]; then
+  npm install
+
+  if pm2 list | grep -q $APP_NAME; then
+    pm2 restart $APP_NAME
+  else
+    pm2 start src/index.js --name $APP_NAME
+  fi
+else
+  echo "No updates found. Nothing to do."
+fi
+
